6b98bd2f3f4564206faeab18a4effea31104091f
David Khourshid <davidkpiano@gmail.com>
2019-01-01 13:38:01 -0500

    Ancestor updates

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ interface Rect extends ClientRect {
   readonly transform?: string;
 }
 
-export function getRect(element: Element, relative: boolean = true): Rect {
+export function getRect(element: HTMLElement, ancestor?: HTMLElement): Rect {
   const {
     top,
     bottom,
@@ -21,9 +21,7 @@ export function getRect(element: Element, relative: boolean = true): Rect {
     height
   } = element.getBoundingClientRect();
 
-  const parentRect = element.parentElement
-    ? element.parentElement.getBoundingClientRect()
-    : undefined;
+  const parentRect = ancestor ? ancestor.getBoundingClientRect() : undefined;
 
   return {
     top: top - (parentRect ? parentRect.top : 0),
@@ -100,22 +98,33 @@ export function getInverse(delta: Delta): Delta {
     heightRatio: 1 / delta.heightRatio
   };
 }
-interface FlipData {
+type FlipData = {
+  state:
+    | "read"
+    | "pre-enter"
+    | "enter"
+    | "pre-move"
+    | "move"
+    | "pre-exit"
+    | "exit";
   key: string;
-  element: HTMLElement;
-  state: string;
-  rect: Rect;
-  styles: StyleMap;
+  element: HTMLElement | undefined;
+  rect: Rect | undefined;
+  styles: StyleMap | undefined;
   delta: Delta;
   inverse: Delta;
   previous: FlipData | undefined;
-}
+};
 
 type FlipElementMap = Record<string, HTMLElement | undefined>;
 
 type FlipListener = (data: FlipData) => void;
 
-export function isVisible(element: HTMLElement) {
+export function isVisible(element?: HTMLElement) {
+  if (!element) {
+    return false;
+  }
+
   const { width, height } = getRect(element);
 
   return !(width === 0 && height === 0);
@@ -147,6 +156,16 @@ export class Flipping {
     this.globalListeners.add(globalListener);
   }
 
+  private findAncestor(element: HTMLElement): HTMLElement | undefined {
+    let currentParent = element.parentElement;
+
+    while (currentParent && !currentParent.hasAttribute(Flipping.keyAttr)) {
+      currentParent = currentParent.parentElement;
+    }
+
+    return currentParent || undefined;
+  }
+
   private set(key: string, data: FlipData): void {
     this.data[key] = data;
 
@@ -154,8 +173,18 @@ export class Flipping {
       listener(data);
     });
 
+    if (data.element) {
+      data.element.dataset.flipState = data.state;
+    } else if (data.previous && data.previous.element) {
+      data.previous.element.dataset.flipState = data.state;
+    }
+    if (!data.rect || !data.styles) {
+      return;
+    }
+
     const distX = Math.abs(data.delta.left);
     const distY = Math.abs(data.delta.top);
+
     Flipping.style(
       data,
       {
@@ -202,6 +231,11 @@ export class Flipping {
     options?: Partial<{ px: boolean }> // TODO
   ): void {
     const { element } = data;
+
+    if (!element) {
+      return;
+    }
+
     const resolvedOptions = { px: true, ...options };
 
     Object.keys(styles).forEach(property => {
@@ -213,17 +247,15 @@ export class Flipping {
         element.style.setProperty(`${fullProperty}-px`, `${value}px`);
       }
     });
-
-    element.dataset.flipState = data.state;
   }
 
   private toElementMap(
     elements: HTMLElement[] | FlipElementMap | undefined
   ): FlipElementMap {
     if (!elements) {
-      elements = Array.from(
+      elements = (Array.from(
         document.querySelectorAll(`[${Flipping.keyAttr}]`)
-      ) as HTMLElement[];
+      ) as HTMLElement[]).filter(el => isVisible(el));
     }
     const map: FlipElementMap = {};
 
@@ -253,13 +285,11 @@ export class Flipping {
         return;
       }
 
-      const visible = isVisible(element);
-
       this.set(key, {
         key,
         element,
-        state: visible ? "read" : "hidden",
-        rect: getRect(element),
+        state: "read",
+        rect: getRect(element, this.findAncestor(element)),
         styles: getStyles(element),
         delta: NO_DELTA,
         inverse: NO_DELTA,
@@ -271,42 +301,63 @@ export class Flipping {
   public flip(): void;
   public flip(elements: HTMLElement[]): void;
   public flip(elements?: HTMLElement[]) {
-    if (!elements) {
-      elements = Array.from(document.querySelectorAll("[data-flip-key]"));
-    }
+    const elementMap = this.toElementMap(elements);
+    const allKeys = new Set(
+      Object.keys(this.data).concat(Object.keys(elementMap))
+    );
 
-    elements.forEach(element => {
-      const key = this.config.getKey(element);
+    allKeys.forEach(key => {
+      const element = elementMap[key];
       let data: FlipData;
       const existingData = this.data[key];
 
       const visible = isVisible(element);
 
-      if (!existingData) {
+      if (!element || !visible) {
+        console.log(">>>", existingData);
+        data = {
+          key,
+          element,
+          state: "exit",
+          rect: element
+            ? getRect(element, this.findAncestor(element))
+            : undefined,
+          styles: element ? getStyles(element) : undefined,
+          delta: NO_DELTA,
+          inverse: NO_DELTA,
+          previous: existingData
+        };
+      } else if (!existingData && element && visible) {
         data = {
           key,
           element,
           state: "enter",
-          rect: getRect(element),
+          rect: getRect(element, this.findAncestor(element)),
           styles: getStyles(element),
           delta: NO_DELTA,
           inverse: NO_DELTA,
           previous: undefined
         };
-      } else {
-        const delta = getDelta(existingData.rect, getRect(element));
+      } else if (existingData && element && visible) {
+        const delta = existingData.rect
+          ? getDelta(
+              existingData.rect,
+              getRect(element, this.findAncestor(element))
+            )
+          : NO_DELTA;
+        // console.log(existingData, getRect(element), delta);
         data = {
           key,
           element,
           state:
-            existingData.state === "hidden"
+            existingData.state === "exit"
               ? visible
                 ? "enter"
-                : "hidden"
+                : "exit"
               : visible
                 ? "move"
                 : "exit",
-          rect: getRect(element),
+          rect: getRect(element, this.findAncestor(element)),
           styles: getStyles(element),
           delta,
           inverse: getInverse(delta),
@@ -315,7 +366,11 @@ export class Flipping {
       }
 
       requestAnimationFrame(() => {
-        this.set(key, { ...data, state: `pre-${data.state}` });
+        this.set(key, {
+          ...data,
+          state: `pre-${data.state}` as "pre-enter" | "pre-move" | "pre-exit"
+        });
+
         requestAnimationFrame(() => {
           this.set(key, data);
         });
@@ -341,12 +396,12 @@ export class Flipping {
       [data-flip-state="pre-move"] {
         transition: none;
         transform: var(--flip-inverse-xy);
-        xclip-path: polygon(0% 0%, calc(var(--flip-iw-ratio) * 100%) 0, calc(var(--flip-iw-ratio) * 100%) calc(var(--flip-ih-ratio) * 100%), 0 calc(var(--flip-ih-ratio) * 100%));
+        --clip-path: polygon(0% 0%, calc(var(--flip-iw-ratio) * 100%) 0, calc(var(--flip-iw-ratio) * 100%) calc(var(--flip-ih-ratio) * 100%), 0 calc(var(--flip-ih-ratio) * 100%));
       }
       [data-flip-state="move"] {
         transition: all .6s ease;
         transform: none;
-        xclip-path: polygon(0% 0%, 100% 0, 100% 100%, 0 100%);
+        --clip-path: polygon(0% 0%, 100% 0, 100% 100%, 0 100%);
       }
     `;
 
