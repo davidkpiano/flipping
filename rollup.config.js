a1e65b71e8f244583e64345a737cfabb2a443a56
David Khourshid <davidkpiano@gmail.com>
2020-06-28 15:29:34 -0400

    Cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -2,34 +2,49 @@ import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 import rollupReplace from 'rollup-plugin-replace';
 import fileSize from 'rollup-plugin-filesize';
+import cleanup from 'rollup-plugin-cleanup';
 
 const createConfig = ({ input, output }) => ({
   input,
   output,
   plugins: [
     rollupReplace({
-      'process.env.NODE_ENV': JSON.stringify('production')
+      'process.env.NODE_ENV': JSON.stringify('production'),
     }),
     typescript({
       clean: true,
       tsconfigOverride: {
         compilerOptions: {
-          declaration: false
-        }
-      }
+          declaration: false,
+        },
+      },
     }),
     terser(),
-    fileSize()
-  ]
+    cleanup({
+      comments: 'none',
+    }),
+    fileSize(),
+  ],
 });
 
-export default [
-  createConfig({
-    input: 'src/index.ts',
-    output: {
-      file: 'dist/flipping.js',
-      format: 'umd',
-      name: 'Flipping'
-    }
-  })
-];
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/flipping.js',
+    format: 'umd',
+    name: 'Flipping',
+  },
+  plugins: [
+    typescript({
+      clean: true,
+      tsconfigOverride: {
+        compilerOptions: {
+          declaration: false,
+        },
+        external: ['tslib'],
+      },
+    }),
+    terser(),
+    fileSize(),
+  ],
+};
